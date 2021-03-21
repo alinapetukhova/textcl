5ddafabbbe85b48c287958f67cee7cdf28be29f0
Alina <erar1@mail.ru>
2021-03-21 11:34:00 +0000

    update setup for pip

--- src/setup.py
+++ src/setup.py
@@ -5,12 +5,26 @@ req = []
 for line in f:
     req.append(line.strip())
 f.close()
-    
 
 setup(name='textcl',
-      version='0.1.0',
-      description='Package for text preprocessing to use in nlp tasks',
       packages=['textcl'],
+      version='0.1.0',
+      license='MIT',
+      description='Package for text preprocessing to use in nlp tasks',
+      author = 'Alina Petukhova',
+      author_email = 'petukhova.alina@gmail.com',
+      url = 'https://github.com/alinapetukhova/textcl',
+      download_url = 'https://github.com/alinapetukhova/textcl/archive/refs/tags/v.0.1.0.tar.gz',
+      keywords = ['NLP', 'Text preprocessing', 'Outlier detection'],
       install_requires=req,
       python_requires='>=3.6', 
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+            'Development Status :: 3 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+      ])
